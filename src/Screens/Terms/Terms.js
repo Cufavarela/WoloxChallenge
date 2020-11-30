1afd632ac15634d88f5ec7263a83b75393411b19
Facundo Varela <cufa.varela@gmail.com>
2020-11-30 20:23:34 -0300

    Terms style, change node-sass version for deploy

--- src/Screens/Terms/Terms.js
+++ src/Screens/Terms/Terms.js
@@ -1,7 +1,25 @@
 import React from "react";
+import BorderButton from "../../Components/Buttons/BorderButton/BorderButton";
+import "./terms.scss";
 
 function Terms() {
-  return <div>Estos son los terminos y condiciones</div>;
+  return (
+    <div className="bg">
+      <div className="container">
+        <h1>
+          <span>🚧 </span>
+          Site under construction!
+          <span> 🚧</span>
+        </h1>
+        <BorderButton
+          path={"/"}
+          text={"Go back"}
+          type={"primary"}
+          size={"big"}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Terms;
